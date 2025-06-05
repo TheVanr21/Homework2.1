28a14cbaba54ab84965d161c2dc3c3df6652f40f
Vasily Rokhlin <rokhlin.vasily@yandex.com>
2025-06-06 01:36:12 +0300

    Рефакторинг

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,36 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.util.List;
 
 public class Main {
+    static StringBuilder log = new StringBuilder();
+
     public static void main(String[] args) {
-        File rootDir = new File("D://Games");
-        StringBuilder log = new StringBuilder();
+        File rootDirectory = new File("D://Games");
 
-        if (rootDir.isDirectory()) {
-            String netologyDir = rootDir + "//Netology";
-            log.append(createDirAndAppendLog(netologyDir));
+        if (rootDirectory.isDirectory()) {
+            List<String> directoriesToCreate = List.of(
+                    rootDirectory + "//Netology",
+                    rootDirectory + "//Netology//src",
+                    rootDirectory + "//Netology//src//main",
+                    rootDirectory + "//Netology//src//test",
+                    rootDirectory + "//Netology//res",
+                    rootDirectory + "//Netology//res//drawables",
+                    rootDirectory + "//Netology//res//vectors",
+                    rootDirectory + "//Netology//res//icons",
+                    rootDirectory + "//Netology//savegames",
+                    rootDirectory + "//Netology//temp"
+            );
 
-            String srcDir = netologyDir + "//src";
-            String resDir = netologyDir + "//res";
-            String savegamesDir = netologyDir + "//savegames";
-            String tempDir = netologyDir + "//temp";
-            log.append(createDirAndAppendLog(srcDir));
-            log.append(createDirAndAppendLog(resDir));
-            log.append(createDirAndAppendLog(savegamesDir));
-            log.append(createDirAndAppendLog(tempDir));
+            for (String directory : directoriesToCreate) {
+                createDirectory(directory);
+            }
 
-            String mainDir = srcDir + "//main";
-            String testDir = srcDir + "//test";
-            log.append(createDirAndAppendLog(mainDir));
-            log.append(createDirAndAppendLog(testDir));
+            createFile(rootDirectory + "//Netology//src//main", "Main.java");
+            createFile(rootDirectory + "//Netology//src//main", "Utils.java");
+            createFile(rootDirectory + "//Netology//temp", "temp.txt");
 
-            log.append(createFileAndAppendLog(mainDir + "//Main.java"));
-            log.append(createFileAndAppendLog(mainDir + "//Utils.java"));
-
-            log.append(createDirAndAppendLog(resDir + "//drawables"));
-            log.append(createDirAndAppendLog(resDir + "//vectors"));
-            log.append(createDirAndAppendLog(resDir + "//icons"));
-
-            String tempFile = tempDir + "//temp.txt";
-            log.append(createFileAndAppendLog(tempFile));
-
-            try (FileWriter writer = new FileWriter(tempFile)) {
+            try (FileWriter writer = new FileWriter(rootDirectory + "//Netology//temp//temp.txt")) {
                 writer.write(log.toString());
             } catch (Exception e) {
                 e.printStackTrace();
@@ -42,18 +38,21 @@ public class Main {
         }
     }
 
-    private static String createDirAndAppendLog(String path) {
+    public static void createDirectory(String path) {
         File dir = new File(path);
-        return "Каталог " + dir.getAbsolutePath() + (dir.mkdir() ? " создан\n" : " не создан\n");
+        if (dir.mkdir()) {
+            log.append("Каталог ").append(dir).append(" создан\n");
+        }
     }
 
-    private static String createFileAndAppendLog(String path) {
-        File file = new File(path);
+    public static void createFile(String path, String filename) {
+        File file = new File(path + "//" + filename);
         try {
-            return "Файл " + file.getAbsolutePath() + (file.createNewFile() ? " создан\n" : " не создан\n");
+            if (file.createNewFile()) {
+                log.append("Файл ").append(file).append(" создан\n");
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            return "Файл " + file.getAbsolutePath() + " не создан\n";
         }
     }
 }
